ef26dd13688b773613cba490e5b6354b81bc0511
JavierBoyaca <68155136+bamg359@users.noreply.github.com>
2024-07-11 20:44:29 -0500

    Clase 11 Julio
    Composición de clases

--- Poo_Python/products/Product.py
+++ Poo_Python/products/Product.py
@@ -1,6 +1,8 @@
 
 
-class product:
+from Category import Category
+
+class Product:
 
     product_id = None
     product_name = None
@@ -23,5 +25,33 @@ class product:
     #Getters and Setters
 
 
-    def create_product(self):
+    products = {}
+
+    def create_product(self, category):
         self.product_id = input("id Producto")
+        self.product_name = input("NOmbre producto")
+        self.category = self.find_category(category)
+        self.description  = input("Descripción")
+        self.price = float(input("Precio"))
+        self.quantity = int(input("Cantidad"))
+        self.brand = input("Marca")
+        self.products[self.product_id] = {"id": self.product_id , "Nombre" :  self.product_name , "Categoria": self.category , "Decripcion": self.description, "Precio": self.price, "Cantidad": self.quantity, "Marca": self.brand}
+
+
+
+    def listing_products(self):
+        for product in self.products.items():
+            print(product)
+
+
+    def find_category(self,category):
+        category_id = int(input("Categoria"))
+        category_list = category.categories.get(category_id)
+        self.category = category_list
+        print(self.category)
+        #value = input("Nombre categoria")
+        #if value in self.category:
+        return self.category
+        #print(self.category)
+
+
